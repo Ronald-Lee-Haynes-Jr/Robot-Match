9ff8e958d06ed052cea234ec1afae3a6e2943c2b
Ronald Lee Haynes Jr <ronald.haynes14@hotmail.com>
2022-09-28 22:52:05 -0400

    Updates

---
+++ precache-manifest.f7e3fe836e942e27ab859831a0863268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e3844012234146e0c2",
+    "url": "/villagefamily/static/css/main.3a13cd87.chunk.css"
+  },
+  {
+    "revision": "d0e3844012234146e0c2",
+    "url": "/villagefamily/static/js/main.d0e38440.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/villagefamily/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "d50cbbdf81c685ded92b",
+    "url": "/villagefamily/static/css/2.191d73fc.chunk.css"
+  },
+  {
+    "revision": "d50cbbdf81c685ded92b",
+    "url": "/villagefamily/static/js/2.d50cbbdf.chunk.js"
+  },
+  {
+    "revision": "06b9c63aa5f02dc9ab2f",
+    "url": "/villagefamily/static/js/runtime~main.06b9c63a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/villagefamily/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2887de4c97f3b2a00a9718e9ad34b6ef",
+    "url": "/villagefamily/index.html"
+  }
+];
