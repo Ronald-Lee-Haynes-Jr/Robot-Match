bc5454c6b21aac2dd78a9b221c5eab3c9bc1539d
Ronald Lee Haynes Jr <ronald.haynes14@hotmail.com>
2022-09-28 20:30:59 -0400

    renamed:    src/Card.js -> src/components/Card.js 	renamed:    src/CardList.js -> src/components/CardList.js 	renamed:    src/Scroll.js -> src/components/Scroll.js 	renamed:    src/SearchBox.js -> src/components/SearchBox.js 	renamed:    src/App.css -> src/containers/App.css 	renamed:    src/App.js -> src/containers/App.js 	renamed:    src/SEGA.woff -> src/containers/SEGA.woff 	modified:   src/index.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '500px'}}>
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
             {props.children}
         </div>
     )
 };
 
-export default Scroll
+export default Scroll;
